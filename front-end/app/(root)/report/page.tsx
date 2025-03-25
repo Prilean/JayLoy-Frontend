bff0c7eae66af203f1ae6fd27ca774dd57313b9e
Apple <apple@Sethas-book.local>
2025-03-25 10:47:40 +0700

    fixed

--- front-end/app/(root)/report/page.tsx
+++ front-end/app/(root)/report/page.tsx
@@ -96,7 +96,7 @@ const Page: React.FC = () => {
       ]
     : [];
 
-  const displayReport = totalReport.slice(0, 2);
+  const displayReport = totalReport.slice(1, 3);
 
   // Fetch user data when component mounts
   const [username, setUsername] = useState("Loading..");
